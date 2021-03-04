387c9a266085c4427cb0197ad848c79c66bfd16e
berger-n <quai.ledrurollin@gmail.com>
2021-03-04 16:07:05 +0100

    dont add UI language change to history

--- src/routes/helpers/observer.js
+++ src/routes/helpers/observer.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react"
 import { useGotoRecoilSnapshot, useRecoilTransactionObserver_UNSTABLE } from "recoil"
 
+const debug = require("debug")("bdrc:observer")
+
 export function TimeTravelObserver() {
   const [snapshots, setSnapshots] = useState([])
 
   useRecoilTransactionObserver_UNSTABLE(({ snapshot }) => {
+    //debug("snap:",snapshot)
+    for (const a of snapshot.getNodes_UNSTABLE(true)) {
+      const info = snapshot.getInfo_UNSTABLE(a)
+      //debug(a,info)
+      // DONE do not not take a snapshot if current change is UI language
+      if (a.key === "uiLangState" && info.isModified) return
+    }
     setSnapshots([...snapshots, snapshot])
   })
 
