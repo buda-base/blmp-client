53d58e9730f4159b6b26c9abdf1687fa4f1c5d1e
berger-n <quai.ledrurollin@gmail.com>
2021-03-08 16:01:49 +0100

    improve tests before creating history snapshot

--- src/routes/helpers/observer.js
+++ src/routes/helpers/observer.js
@@ -3,6 +3,53 @@ import { useGotoRecoilSnapshot, useRecoilTransactionObserver_UNSTABLE } from "re
 
 const debug = require("debug")("bdrc:observer")
 
+const sameFieldModifiedAgain = (m, s1, s2) => {
+  if (!s2 || !s2.a) return true
+  const m_content_tab = m.info.loadable.getValue(),
+    s1_content_tab = s1.info.loadable.getValue(),
+    s2_content_tab = s2.info.loadable.getValue()
+  for (const i in m_content_tab) {
+    const m_content = m_content_tab[i],
+      s1_content = s1_content_tab[i],
+      s2_content = s2_content_tab[i]
+    //debug("diff",m_content,s1_content,s2_content)
+    for (const k of Object.keys(m_content)) {
+      //debug("k",k)
+      const tags = ["@id", "@value", "@language", "type"]
+      if (typeof m_content[k] === "object") {
+        for (const tag of tags) {
+          if (m_content[k][tag] != s1_content[k][tag]) {
+            if (s2_content && s2_content[k]) {
+              if (tag == "@language" || s1_content[k][tag] == s2_content[k][tag]) {
+                //debug("false", tag)
+                return false
+              } else if (s1_content[k][tag] != s2_content[k][tag]) {
+                //debug("true", tag)
+                return true
+              }
+            }
+          }
+        }
+      } else if (typeof m_content[k] === "string") {
+        if (tags.includes(k)) {
+          if (m_content[k] != s1_content[k]) {
+            if (s2_content && s2_content[k]) {
+              if (k == "@language" || s1_content[k] == s2_content[k]) {
+                //debug("false", k)
+                return false
+              } else if (s1_content[k] != s2_content[k]) {
+                //debug("true", k)
+                return true
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return false
+}
+
 export function TimeTravelObserver() {
   const [snapshots, setSnapshots] = useState([])
 
@@ -23,14 +70,22 @@ export function TimeTravelObserver() {
 
     // DONE use only one snapshot for all successive modifications of same property value
     if (snapshots.length && modified.length === 1) {
-      debug("modif:", modified)
-      for (const a of snapshots[snapshots.length - 1].getNodes_UNSTABLE(true)) {
-        const info = snapshots[snapshots.length - 1].getInfo_UNSTABLE(a)
+      const nodes1 = Array.from(snapshots[snapshots.length - 1].getNodes_UNSTABLE(true))
+      const nodes2 =
+        snapshots.length > 1 ? Array.from(snapshots[snapshots.length - 1 - 1].getNodes_UNSTABLE(true)) : null
+      //debug("modif:", modified,modified[0].info.loadable.getValue(),nodes1,nodes2)
+      for (const i in nodes1) {
+        const s1 = nodes1[i]
+        const s2 = nodes2 ? nodes2[i] : null
+        const info1 = snapshots[snapshots.length - 1].getInfo_UNSTABLE(s1)
+        const info2 = s2 ? snapshots[snapshots.length - 1 - 1].getInfo_UNSTABLE(s2) : null
         // DONE also create new state when new or deleted prefLabel
         if (
-          info.isModified &&
-          a.key === modified[0].a.key &&
-          info.loadable.contents.length === modified[0].info.loadable.contents.length
+          info1.isModified &&
+          s1.key === modified[0].a.key &&
+          info1.loadable.contents.length === modified[0].info.loadable.contents.length &&
+          // DONE check changes in sub/property based on @value, @id, @language, type
+          sameFieldModifiedAgain(modified[0], { a: s1, info: info1 }, s2 ? { a: s2, info: info2 } : null)
         ) {
           // replace previous snapshot for same property
           snapshots[snapshots.length - 1] = snapshot
