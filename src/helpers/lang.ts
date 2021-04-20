e7a7172c5a4cb5f277843d88eaa1e7fda5d79be9
eroux <elie.roux@telecom-bretagne.eu>
2021-04-20 17:25:25 +0200

    import jsewts, use it when UI language is bo

--- src/helpers/lang.ts
+++ src/helpers/lang.ts
@@ -1,10 +1,13 @@
+import "./jsewts.d.ts"
+
+import { fromWylie } from "jsewts"
+
 const debug = require("debug")("bdrc:rdf:lang")
 
 export const ValueByLangToStrPrefLang = (vbl: Record<string, string>, prefLang: string) => {
   if (prefLang in vbl) return vbl[prefLang]
   if (prefLang === "bo" && "bo-x-ewts" in vbl) {
-    // TODO: convert to Unicode from EWTS
-    return vbl["bo-x-ewts"]
+    return fromWylie(vbl["bo-x-ewts"])
   }
   if ("en" in vbl) return vbl["en"]
 
