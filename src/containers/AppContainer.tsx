825b69811d86e210eeb13478bf34b45d21048dd2
berger-n <quai.ledrurollin@gmail.com>
2021-07-22 17:11:04 +0200

    home

--- src/containers/AppContainer.tsx
+++ src/containers/AppContainer.tsx
@@ -152,14 +152,43 @@ function App(props: AppProps) {
     }, delay)
   }
 
+  debug(props)
+
   return (
-    <div ref={appEl} /*onClick={updateUndo} onKeyUp={updateUndo}*/>
+    <div
+      ref={appEl}
+      /*onClick={updateUndo} onKeyUp={updateUndo}*/ className={props.location.pathname === "/" ? "home" : ""}
+    >
       <NavBarContainer />
       <main>
         <div>
           <EntitySelector />
           <Switch>
-            <Route exact path="/" component={NewEntityContainer} />
+            <Route
+              exact
+              path="/"
+              /* component={NewEntityContainer} */ render={(props) => {
+                return (
+                  <div className="centered-ctn">
+                    <div>
+                      <h1>Welcome!</h1>
+                      <span>BDCR Editor</span>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus
+                        tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
+                        Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec
+                        nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis
+                        arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus
+                        volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
+                        Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante
+                        ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac
+                        mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                      </p>
+                    </div>
+                  </div>
+                )
+              }}
+            />
             <Route exact path="/profile" component={ProfileContainer} />
             <Route exact path="/new" component={NewEntityContainer} />
             <Route exact path="/new/:shapeQname" component={EntityCreationContainer} />
