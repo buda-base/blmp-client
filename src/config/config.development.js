12a74a0c4ae906fe2a32c7e539ab4b5c46da3e43
berger-n <quai.ledrurollin@gmail.com>
2022-01-25 12:15:08 +0100

    switch /simplesearch url back to online

--- src/config/config.development.js
+++ src/config/config.development.js
@@ -4,8 +4,8 @@ const config = {
   env: process.env.NODE_ENV || "development",
   SITE_URL: "http://localhost:3001",
   requireAuth: true,
-  LIBRARY_URL: "http://localhost:3000",
-  //LIBRARY_URL: "http://library.bdrc.io",
+  //LIBRARY_URL: "http://localhost:3000",
+  LIBRARY_URL: "http://library.bdrc.io",
   //TEMPLATES_BASE: "http://localhost:8080/",
   TEMPLATES_BASE: "http://purl.bdrc.io/",
 }
