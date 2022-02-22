5f191c60ab00c15844967b117e2afe042829dcf3
eroux <elie.roux@telecom-bretagne.eu>
2022-02-22 14:50:33 +0100

    fix base url

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -2,7 +2,7 @@ const config = {
   __DEV__: false,
   API_BASEURL: "https://editserv-dev.bdrc.io/",
   env: process.env.NODE_ENV || "production",
-  SITE_URL: "https://buda-editor.msblabs.us",
+  SITE_URL: "https://editor.bdrc.io",
   requireAuth: true,
   LIBRARY_URL: "https://library.bdrc.io",
   TEMPLATES_BASE: "https://purl.bdrc.io/",
