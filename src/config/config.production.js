d589c3fb87939461e7386a8fb140b4a9ad362503
berger-n <quai.ledrurollin@gmail.com>
2022-02-22 14:44:30 +0100

    update prod config

--- src/config/config.production.js
+++ src/config/config.production.js
@@ -1,9 +1,9 @@
 const config = {
   __DEV__: false,
-  API_BASEURL: "https://editserv-dev.bdrc.io/",
+  API_BASEURL: "http://editserv-dev.bdrc.io/",
   env: process.env.NODE_ENV || "production",
   SITE_URL: "https://buda-editor.msblabs.us",
-  requireAuth: false,
+  requireAuth: true,
   LIBRARY_URL: "https://library.bdrc.io",
   TEMPLATES_BASE: "https://purl.bdrc.io/",
 }
