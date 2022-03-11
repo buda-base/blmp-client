e3f9dd8195b8536ef5a3bb6f9ab67d5c8bea2731
berger-n <quai.ledrurollin@gmail.com>
2022-03-11 17:32:34 +0100

    dont show confirmation popup when reloading after scan request

--- src/helpers/rdf/io.ts
+++ src/helpers/rdf/io.ts
@@ -394,7 +394,7 @@ export function EntityFetcher(entityQname: string, shapeRef: RDFResourceWithLabe
       const localEntities = await getUserLocalEntities(auth0)
       // 1 - check if entity has local edits (once shape is defined)
       //debug("local?", entityQname, localEntities[entityQname])
-      if (shapeRef && localEntities[entityQname] !== undefined) {
+      if (reloadEntity !== entityQname && shapeRef && localEntities[entityQname] !== undefined) {
         useLocal = window.confirm("found previous local edits for this resource, load them?")
         const store: rdf.Store = rdf.graph()
         if (useLocal) {
